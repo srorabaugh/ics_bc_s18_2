d7f60097d1c475b3686b5d025c088f36c885533f
srorabaugh <stevendrm29@gmail.com>
2018-06-14 22:47:31 -0700

    Complete age_in_seconds exercise

---
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -0,0 +1,3 @@
+puts 'What is your favorite number?'
+nmbr = gets.chomp
+puts (nmbr.to_i + 1).to_s + ' is a bigger and better favorite number. I would recommend reconsidering your favorite number.'
---
+++ week1/ch05/full_name_greeting.rb
@@ -0,0 +1,7 @@
+puts 'Hello, what is your first name?'
+fname = gets.chomp
+puts 'And your middle name?'
+mname = gets.chomp
+puts 'And your last name?'
+lname = gets.chomp
+puts 'Nice to meet you, ' +fname + ' ' + mname + ' ' + lname + '!'
