6fe8ad7ac5d185df371fca4ef215bc53e80055c0
srorabaugh <stevendrm29@gmail.com>
2018-06-21 23:48:52 -0700

    Complete Table of Contents Exercise

---
+++ week2/ch06/table_of_contents.rb
@@ -0,0 +1,6 @@
+line_width = 45
+puts ('Table of Contents'.center(line_width))
+puts ''
+puts ('Chapter 1: Getting Started'.ljust(line_width)) + ('Page 1 '.rjust(line_width))
+puts ('Chapter 2: Numbers'.ljust(line_width)) + ('Page 9 '.rjust(line_width))
+puts ('Chapter 3: Letters'.ljust(line_width)) + ('Page 13'.rjust(line_width))
