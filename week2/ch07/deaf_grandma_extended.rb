e5e82d1127817c96fc31f78b529108018979d78b
srorabaugh <stevendrm29@gmail.com>
2018-06-22 22:41:37 -0700

    Complete Deaf Grandma Extended Exercise

---
+++ week2/ch07/deaf_grandma_extended.rb
@@ -0,0 +1,24 @@
+count = 1
+while count != 4
+  resp = gets.chomp
+ if resp == 'BYE'
+    count = count + 1
+  if resp == 'BYE' && count !=4
+    puts "WHAT WAS THAT SONNY?"
+  end
+ else
+   count = 1
+ end
+ if resp == resp.upcase && resp != 'BYE'
+    puts 'NO, NOT SINCE ' + (rand(30) + 1920).to_s + '!'
+  end
+  if resp != resp.upcase
+    puts 'HUH?!  SPEAK UP, SONNY!'
+  end
+end
+  while count = 4
+  if resp == 'BYE'
+    puts 'BYE SONNY'
+    break
+  end
+end
