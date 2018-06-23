54e0fd05f3a86d6d6a5f860d1ae0d1b5cb362c03
srorabaugh <stevendrm29@gmail.com>
2018-06-22 22:40:14 -0700

    Edit Deaf Grandma Exercise

--- week2/ch07/deaf_grandma.rb
+++ week2/ch07/deaf_grandma.rb
@@ -1,9 +1,13 @@
-input = ''
+input = ' '
 while true
   resp = gets.chomp
-  if resp == resp.upcase
+  if resp == resp.upcase && resp!= 'BYE'
     puts 'NO, NOT SINCE ' + (1930 + (rand(21))).to_s + '!'
-  else
+  end
+  if resp == 'BYE'
+    puts 'BYE SONNY'
+  end
+  if resp != resp.upcase && resp!= 'BYE'
     puts 'HUH?! SPEAK UP, SONNY!'
   end
     if resp == 'BYE'
