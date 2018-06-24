d6b6f972916d64845502b6a5d3e9d246a9a0b02e
srorabaugh <stevendrm29@gmail.com>
2018-06-23 21:03:39 -0700

    Edit New Table of Contents Exercise

--- week2/ch08/table_of_contents.rb
+++ week2/ch08/table_of_contents.rb
@@ -4,5 +4,5 @@ puts ''
 chapters = ['"Chapter 1: Getting Started"','"Chapter 2: Numbers"', '"Chapter 3: Letters"']
 pages = ['1', '9', '13']
 chapters.zip(pages) do |chp, pgs|
-puts "#{chp.ljust(line_width)} #{pgs.ljust(line_width)}"
+puts "#{chp.ljust(line_width)} #{pgs.rjust(line_width)}"
 end
