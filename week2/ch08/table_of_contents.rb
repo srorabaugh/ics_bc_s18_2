26ad0a69cdcf5964c2a50e9540436e03611fc0fb
srorabaugh <stevendrm29@gmail.com>
2018-06-27 14:55:50 -0700

    Correct Table of Contents 2 Exercise

--- week2/ch08/table_of_contents.rb
+++ week2/ch08/table_of_contents.rb
@@ -1,5 +1,5 @@
 line_width = 45
-puts ('Table of Contents'.center(line_width))
+puts ('Table of Contents'.center(line_width*2))
 puts ''
 chapters = ['"Chapter 1: Getting Started"','"Chapter 2: Numbers"', '"Chapter 3: Letters"']
 pages = ['1', '9', '13']
