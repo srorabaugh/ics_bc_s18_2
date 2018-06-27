ee4838f75cabcca293ea72173b1b89134bc38e4c
srorabaugh <stevendrm29@gmail.com>
2018-06-27 14:57:19 -0700

    Correct Deaf Grandma 1 Exercise

--- week2/ch07/deaf_grandma.rb
+++ week2/ch07/deaf_grandma.rb
@@ -3,12 +3,11 @@ while true
   resp = gets.chomp
   if resp == resp.upcase && resp!= 'BYE'
     puts 'NO, NOT SINCE ' + (1930 + (rand(21))).to_s + '!'
-  end
+  elsif resp != resp.upcase
+      puts 'HUH?! SPEAK UP, SONNY!'
+    end
   if resp == 'BYE'
     puts 'BYE SONNY'
-  end
-  if resp != resp.upcase && resp!= 'BYE'
-    puts 'HUH?! SPEAK UP, SONNY!'
   end
     if resp == 'BYE'
       break
